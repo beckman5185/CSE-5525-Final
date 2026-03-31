1e8aecc23ece85f1d027ff34ca20f79d20921763
Carter Glazer <carterglazer@pitzer-rw01.ten.osc.edu>
2026-03-31 09:10:37 -0400

    sft-4 tests

---
+++ evals/run_eval.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#This part is need for OSC users
+export CC=gcc
+export CXX=g++
+export TRITON_CACHE_DIR=/fs/scratch/PAS3272/${USER}/triton_cache
+
+
+export UV_CACHE_DIR=/fs/scratch/PAS3272/${USER}/.cache/uv  #control your uv caches
+
+# Dummy key to prevent import error in safety-eval (WildGuard doesn't actually use it)
+export OPENAI_API_KEY="sk-dummy-not-used"
+
+# Disable vLLM V1 multiprocessing so EngineCore runs inline in the spawned subprocess
+# rather than forking a grandchild process that loses CUDA visibility on SLURM
+export VLLM_ENABLE_V1_MULTIPROCESSING=0
+
+
+cd olmes/oe_eval/dependencies/safety
+bash install.sh
+
+
+
+dataset_name=(
+    "gsm8k"
+    "mbpp"
+    "ifeval"
+    "harmbench::default"
+    "xstest::default"
+
+)
+model_path=/users/PAS2526/carterglazer/glazer77/CSE-5525-Final/sft-4
+
+for dataset in "${dataset_name[@]}"; do
+    echo "Evaluating on ${dataset}..."
+
+    uv run olmes \
+        --model ${model_path} \
+        --task ${dataset} \
+        --output-dir $model_path-eval-${dataset} 
+done
