43f5fbf32a0fed76266f730b795c2e904dfeb633
Kathleen Duffey <duffey.98@buckeyemail.osu.edu>
2026-04-18 14:39:43 -0400

    Fix print statement to show full chat template
    Changed to show full chat template

--- fix.py
+++ fix.py
@@ -6,4 +6,4 @@ model_path = "/users/PAS2526/carterglazer/glazer77/CSE-5525-Final/sft-4"  # upda
 tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.2-1B")
 tokenizer.chat_template = "{% for message in messages %}{{ message['role'].capitalize() }}: {{ message['content'] }}\n\n{% endfor %}{% if add_generation_prompt %}Assistant:{% endif %}"
 tokenizer.save_pretrained(model_path)
-print("Done:", tokenizer.chat_template[:80])
+print("Done:", tokenizer.chat_template)
